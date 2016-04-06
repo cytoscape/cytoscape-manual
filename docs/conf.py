ea3a6e017f35c824ef7ad5b35fb804f68d9228e5
Barry Demchak <idekerlab.bdemchak@gmail.com>
2016-04-05 20:45:56 -0700

    table experiments

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ pygments_style = 'sphinx'
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_style = 'css/test.css'
+html_style = 'docs/css/test.css'
 
 html_static_path = ['_static']
 html_show_sourcelink = True
