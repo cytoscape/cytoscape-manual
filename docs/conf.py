36bba60ed49c299d26de8613dcd6eb853408d4da
Barry Demchak <idekerlab.bdemchak@gmail.com>
2016-04-27 16:35:10 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ texinfo_documents = [
 from recommonmark.parser import CommonMarkParser
 
 # The suffix of source filenames.
-source_suffix = ['.md', '.rst']
+source_suffix = ['.rst', '.md']
 
 source_parsers = {
 	'.md': CommonMarkParser,
