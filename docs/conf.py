3958c70dcbbdf3c93381339900ce78cb7b8e0850
Brett Settle <brettjsettle@gmail.com>
2018-10-22 11:16:30 -0700

    Update from 3.6.0 to 3.7.0

--- docs/conf.py
+++ docs/conf.py
@@ -7,8 +7,8 @@ project = u'Cytoscape User Manual'
 copyright = u'2017, The Cytoscape Consortium'
 author = u'The Cytoscape Consortium'
 
-version = '3.6.0'
-release = '3.6.0'
+version = '3.7.0'
+release = '3.7.0'
 language = None
 
 exclude_patterns = ['_build']
