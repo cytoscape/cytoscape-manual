956f3b70754d79fbe25502f76af88edc3278d690
Kristina Hanspers <kristina.hanspers@gladstone.ucsf.edu>
2020-10-26 16:38:38 -0700

    updated version to 3.8.2

--- docs/conf.py
+++ docs/conf.py
@@ -7,8 +7,8 @@ project = u'Cytoscape User Manual'
 copyright = u'2020, The Cytoscape Consortium'
 author = u'The Cytoscape Consortium'
 
-version = '3.8.1'
-release = '3.8.1'
+version = '3.8.2'
+release = '3.8.2'
 language = None
 
 exclude_patterns = ['_build']
