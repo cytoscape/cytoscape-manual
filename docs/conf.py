b34f91d761472d874a9a84b688aec5a9bcc4a949
Kristina Hanspers <kristina.hanspers@gladstone.ucsf.edu>
2023-01-30 13:34:10 -0800

    update version and release

--- docs/conf.py
+++ docs/conf.py
@@ -7,8 +7,8 @@ project = u'Cytoscape User Manual'
 copyright = u'2021, The Cytoscape Consortium'
 author = u'The Cytoscape Consortium'
 
-version = '3.9.1'
-release = '3.9.1'
+version = '3.10'
+release = '3.10'
 language = None
 
 exclude_patterns = ['_build']
