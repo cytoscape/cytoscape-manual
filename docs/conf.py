ec07bf612ad32e5495bbf8c36f4004ecc5c11cd9
Kristina Hanspers <kristina.hanspers@gladstone.ucsf.edu>
2023-11-20 12:23:38 -0800

    Update conf.py
    updated how stylesheet are handled

--- docs/conf.py
+++ docs/conf.py
@@ -17,11 +17,12 @@ pygments_style = 'sphinx'
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-def setup(app):
-  app.add_stylesheet( "css/tables.css" )
+#def setup(app):
+  #app.add_stylesheet( "css/tables.css" ) ##deprecated method
 # from http://stackoverflow.com/questions/32079200/how-do-i-set-up-custom-styles-for-restructuredtext-sphinx-readthedocs-etc/32079202#32079202
 
 html_static_path = ['_static']
+html_css_files = ['css/tables.css']
 html_show_sourcelink = True
 htmlhelp_basename = 'Testdoc'
 html_logo = '_static/images/cytoscape3-icon-trans-128x128.png'
